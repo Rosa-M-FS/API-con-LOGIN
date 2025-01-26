ccc2028d226d89a6e3c1f4bfa7ad713922d93511
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-01-26 18:35:59 +0100

    Ejercicio Terminado

--- crypto/config.js
+++ crypto/config.js
@@ -4,4 +4,4 @@ const crypto=require('crypto');
 const secret = crypto.randomBytes(64).toString('hex');
 const hashedSecret = bcrypt.hashSync(secret, 10);
 
-module.exports = hashedSecret;
+module.exports = hashedSecret; 
